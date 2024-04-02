e486871bf1114dc1598289795ef0e9067d30025e
mohdshahbaz370 <76890051+mohdshahbaz370@users.noreply.github.com>
2024-04-02 06:48:10 +0530

    mongodb database connected

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 const app = express();
 
 app.listen(3000, () => console.log("server is running on port 3000"));
