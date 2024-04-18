6bfd8ec128d0cd0daea9c62bd8762091c9926e6b
mohdshahbaz370 <76890051+mohdshahbaz370@users.noreply.github.com>
2024-04-18 17:31:39 +0530

    import issue fixed of index.js backend and added new company name inside header

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
 const listingRouter = require("./routes/listing.route");
 const cookieParser = require("cookie-parser");
-import path from "path";
+const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 mongoose
