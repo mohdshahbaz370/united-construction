2617f053f340a057ad0564589738918355af9c9c
mohdshahbaz370 <76890051+mohdshahbaz370@users.noreply.github.com>
2024-04-18 17:09:23 +0530

    build and deployment more fixes in index page backend and in root folder package.json

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use("/api/listing", listingRouter);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use((error, req, res, next) => {
